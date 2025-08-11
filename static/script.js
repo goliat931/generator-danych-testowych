625050052be596ced04005d48733348c4d6ca539
Dawid Kaniewski <goliat931@gmail.com>
2025-08-11 09:46:11 +0200

    Aktualizacja do wersji statycznej

--- static/script.js
+++ static/script.js
@@ -4,24 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (generateBtn && peselOutput) {
         generateBtn.addEventListener('click', () => {
-            fetch('/generate-pesel')
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    if (data && data.pesel) {
-                        peselOutput.innerText = data.pesel;
-                    } else {
-                        peselOutput.innerText = 'Błąd: Nie otrzymano numeru PESEL';
-                    }
-                })
-                .catch(error => {
-                    console.error('There was a problem with the fetch operation:', error);
-                    peselOutput.innerText = 'Błąd serwera. Sprawdź konsolę, aby uzyskać więcej informacji.';
-                });
+            // Generowanie losowego PESEL w JS
+            let pesel = '';
+            for (let i = 0; i < 11; i++) {
+                pesel += Math.floor(Math.random() * 10);
+            }
+            peselOutput.innerText = pesel;
         });
     }
 });
